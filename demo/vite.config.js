19e54e9e8a7e15863898a4e70ec841d220669c60
Richard Van Camp <richard.vancamp@monad.fi>
2021-08-14 12:24:15 +0300

    Fix: correct path to gh pages

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -3,10 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+    base: mode === 'production' ? '/svelte-sliders/' : '/',
     plugins: [
         svelte({
             preprocess: sveltePreprocess(),
         }),
     ],
-});
+}));
